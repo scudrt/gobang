b0ab75a41a4157e2ce4508a26dc40b2d19c5b6f0
SCUDRT <1208044562@qq.com>
2017-12-17 15:36:28 +0800

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -2,9 +2,20 @@
 #include <graphics.h>
 #include "gameTrainer.h"
 #include "gameContainer.h"
-using namespace container;
-#define WINDOWSIZE_X 764 //1024
-#define WINDOWSIZE_Y 512 //640
+//window's size
+#define WINDOWSIZE_X 680
+#define WINDOWSIZE_Y 680
+//map's size
+#define MAP_X 15
+#define MAP_Y 15
+#define START_X 40
+#define START_Y 40
+#define END_X 640
+#define END_Y 640
+//false presents human, true presents AI
+#define PLAYER_0 false
+#define PLAYER_1 false
+GameDirector *director;
 void loadFile()
 {
 	;
@@ -16,39 +27,46 @@ void saveFile()
 void init()
 {
 	loadFile();
+
 	initgraph(WINDOWSIZE_X,WINDOWSIZE_Y,0);
+
+	director = new GameDirector(
+	Position(MAP_X,MAP_Y),Position(START_X,START_Y),Position(END_X,END_Y));
+	director->addPlayer(PLAYER_0);
+	director->addPlayer(PLAYER_1);
 }
 void switchPlayer()
 {
-	;
+	director->changePlayer();
 }
 void play()
 {
-	;
+	Position pos;
+	do
+	{
+		pos=director->getCurrentPlayer()->getXY();
+	}while(director->putChess(pos) == false);
 }
-void draw()
+bool isEnded()
 {
-	;
-}
-bool judge()
-{
-	;
+	return director->isTheGameEnded();
 }
 void tidyUp()
 {
+	director->showResult();
 	saveFile();
 	closegraph();
+	delete director;
 }
 int main()
 {
 	init();
 	do
 	{
-		draw();
 		switchPlayer();
 		play();
 	}
-	while (judge());
+	while (!isEnded());
 	tidyUp();
     return 0;
 }
